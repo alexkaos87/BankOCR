1ce49b69d9ec2fe825ebf6e72876b5b40b2accfe
Alessio Coppola <alessio.coppola@capgemini.com>
2023-05-13 12:48:29 +0200

    review python script

--- tools/GreenswToXml.py
+++ tools/GreenswToXml.py
@@ -1,4 +1,5 @@
 import argparse
+from calendar import c
 import enum
 import sys
  
@@ -30,7 +31,7 @@ class Error:
         
         return error
 
-    def toXml(self, linePreamble):
+    def toXmlCppcheckStyle(self, linePreamble):
         buffer = linePreamble + f"<error id=\"{self.code}\" severity=\"{self.severity}\" msg=\"{self.description}\" verbose=\"{self.verbose}\" file0=\"{self.file}\">\n"
         buffer = buffer + linePreamble + f"\t<location file=\"{self.file}\" line=\"{self.line}\"/>\n"
         buffer = buffer + linePreamble + "</error>\n"
@@ -41,8 +42,6 @@ def detectErrors(fileName):
     errors = []
 
     tableState = TableState(1)
-    isHeader = False
-    isElement = True
     reader = open(fileName, 'r')
     for line in reader:
         if "+---------+-------------------------+------+---------------------------+" in line and tableState is TableState(1):
@@ -63,6 +62,57 @@ def detectErrors(fileName):
 
     return errors
 
+def generateXmlReportCppcheckStyle(inputFile, outputFile):
+    errors = detectErrors(inputFile)
+
+    writer = open(outputFile, 'w')
+    writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+    writer.write("<results version=\"1\">\n")
+    writer.write("\t<GreenSW version=\"0.8\"/>\n")
+    writer.write("\t<errors>\n")
+
+    for error in errors:
+        writer.write(error.toXmlCppcheckStyle("\t\t"))
+
+    writer.write("\t</errors>\n")
+    writer.write("</results>\n")
+
+def generateXmlReportGtestStyle(inputFile, outputFile):
+    errors = detectErrors(inputFile)
+
+    errorsMap =  {}
+    for error in errors:
+        if not error.file in errorsMap: # key is not available
+            errorsMap[error.file] = []
+        
+        errorsMap[error.file].append(error)
+
+    writer = open(outputFile, 'w')
+    writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+
+    globalDetectsNumber = len(errors)
+    writer.write(f"<testsuites tests=\"{globalDetectsNumber}\" failures=\"{globalDetectsNumber}\" disabled=\"0\" errors=\"0\" time=\"0\" timestamp=\"0\" name=\"AllSustainabilityErrors\">\n")
+    
+    for errorKey in errorsMap:
+        defectFile = errorKey
+        errorList = errorsMap[errorKey]
+        defectsNumber = len(errorList)
+        writer.write(f"\t<testsuite name=\"{defectFile}\" tests=\"{defectsNumber}\" failures=\"{defectsNumber}\" disabled=\"0\" skipped=\"0\" errors=\"0\" time=\"0\" timestamp=\"0\">\n")
+    
+        for error in errorList:
+            testName = error.code
+            className = error.file
+            writer.write(f"\t\t<testcase name=\"{testName}\" status=\"run\" result=\"completed\" time=\"0\" timestamp=\"0\" classname=\"{className}\">\n")
+    
+            message = f"{error.file}:{error.line}\t{error.description}"
+            writer.write(f"\t\t\t<failure message=\"{message}\" type\"\"></failure>\n")
+    
+            writer.write(f"\t\t</testcase>\n")
+    
+        writer.write(f"\t</testsuite>\n")
+    
+    writer.write(f"</testsuites>\n")
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
@@ -71,16 +121,4 @@ if __name__ == "__main__":
 
     print("fileName: {}".format(args.fileName))
 
-    errors = detectErrors(args.fileName)
-
-    writer = open("greensw-results.xml", 'w')
-    writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
-    writer.write("<results version=\"1\">\n")
-    writer.write("\t<GreenSW version=\"0.8\"/>\n")
-    writer.write("\t<errors>\n")
-
-    for error in errors:
-        writer.write(error.toXml("\t\t"))
-
-    writer.write("\t</errors>\n")
-    writer.write("</results>\n")
+    generateXmlReportGtestStyle(args.fileName, "greensw-results.xml")
