1919837bcb9796fce0e69a7ce938435849be8238
Armandodal <134075460+Armandodal@users.noreply.github.com>
2023-05-24 11:38:57 +0200

    Update provaOCR.cpp

--- provaOCR.cpp
+++ provaOCR.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Ciao Jenkins 24_05 second time"" << std::endl;
+  std::cout << "Ciao Jenkins 24_05 third time"" << std::endl;
     
   return 0;
 }
